ba1c76f03f247d50def67f81cbf29e9e5214d3b0
Goodman599 <Gonghm2007@gmail.com>
2026-01-31 10:09:14 -0800

    WIP

--- Scripts/base_visitor.gd
+++ Scripts/base_visitor.gd
@@ -8,17 +8,27 @@ class_name BaseVisitor
 #	The keys are the "names of the branch" and the value is an array of dialogues.
 var dialogues : Dictionary
 
+# Name of the visitor; Used to load dialogue file
+@export var visitor_name : String
+
 # Visitors need a reference to the dialogue box to show their text
 @export var dialogue_box : DialogueBox
 
 # Variables to keep track of which dialogue is currently being shown
+var current_visit_branch : String
 var current_dialogue_branch : String
 var current_dialogue_index : int = 0
+var current_dialogue_line_count : int
 
-# Connect the 2 buttons on the dialogue_box scene to the dialogue managers
 func _ready():
+	# Connect the 2 buttons on the dialogue_box scene to the dialogue managers
 	dialogue_box.get_node("Next").pressed.connect(next_dialogue)
 	dialogue_box.get_node("Back").pressed.connect(prev_dialogue)
+	
+	# Load dialogue from json
+	dialogues = load_json_as_dict("res://Dialogues/" + visitor_name + ".json")
+	print(dialogues)
+	print("res://Dialogues/" + visitor_name + ".json")
 
 
 # Takes a String, formats it, and puts it into the dialogue box
@@ -33,14 +43,78 @@ func plain_to_clickable(text : String) -> String:
 	return text
 
 # Switch to the next dialogue in the current branch
-# TODO: No index out of bounds detections
 func next_dialogue():
-	current_dialogue_index += 1
-	show_text(dialogues[current_dialogue_branch][current_dialogue_index])
+	if current_dialogue_index < current_dialogue_line_count:
+		# If just submitted
+		if current_dialogue_index == current_dialogue_line_count - 1:
+			check_submissions()
+			return
+		
+		current_dialogue_index += 1
+		show_text(dialogues[current_visit_branch][current_dialogue_branch]["lines"][current_dialogue_index])
+		
+		# Show any input boxes
+		if current_dialogue_index == current_dialogue_line_count - 1:
+			for requirement : String in dialogues[current_visit_branch][current_dialogue_branch]["accepts"]:
+				dialogue_box.get_node("Next").text = "Submit"
+				create_submission_box(requirement)
 
 
 # Switch to the previous dialogue in the current branch
-# TODO: No index out of bounds detections
 func prev_dialogue():
-	current_dialogue_index -= 1
-	show_text(dialogues[current_dialogue_branch][current_dialogue_index])
+	if current_dialogue_index > 0:
+		current_dialogue_index -= 1
+		show_text(dialogues[current_visit_branch][current_dialogue_branch]["lines"][current_dialogue_index])
+		
+		# Exiting out of submission line
+		if current_dialogue_index == current_dialogue_line_count - 2:
+			dialogue_box.get_node("Next").text = "Next"
+
+
+# Sets the visit branch to the given string
+# Also sets the dialogue branch to dialogue0
+func set_visit_branch(branch_name : String) -> void:
+	current_visit_branch = branch_name
+	set_dialogue_branch("dialogue0")
+
+# Sets the dialogue branch to the given string
+# Also initializes the line count
+# Also display's the dialogue as text
+func set_dialogue_branch(branch_name : String):
+	current_dialogue_branch = branch_name
+	current_dialogue_line_count = dialogues[current_visit_branch][current_dialogue_branch]["lines"].size()
+	show_text(dialogues[current_visit_branch][current_dialogue_branch]["lines"][current_dialogue_index])
+
+# Takes a file path and returns a json as a dictionary
+func load_json_as_dict(path: String) -> Dictionary:
+	var file := FileAccess.open(path, FileAccess.READ)
+	if file == null:
+		push_error("Failed to open file: " + path + ". Make sure the name of the visitor is set correctly")
+		return {}
+	var json_text := file.get_as_text()
+	file.close()
+	var json := JSON.new()
+	var err := json.parse(json_text)
+	if err != OK:
+		push_error("JSON parse error: %s at line %d" % [json.get_error_message(), json.get_error_line()])
+		return {}
+	return json.data
+
+
+func queue_visit():
+	pass
+
+# Goes through every child (Which should only be submission boxes
+func check_submissions():
+	for child in get_children():
+		if child is SubmissionEventBox or child is SubmissionPersonBox:
+			print(child.get_card_data())
+	
+
+func create_submission_box(type : String):
+	var box
+	if type == "person":
+		box = load("res://Scenes/submission_person_box.tscn")
+	elif type == "event":
+		box = load("res://Scenes/submission_event_box.tscn")
+	add_child(box.instantiate())
--- Scripts/visitor_manager.gd
+++ Scripts/visitor_manager.gd
@@ -1,30 +1,30 @@
 extends Node
 class_name VisitorManager
 
-var day = 1 # 1 - 28
+var time = 0 # 0 - 41, one for each day and night
 
-visitor_queue = [VisitorInstance1, VisitorInstance2, VisitorInstance3 ]
+var visitor_queue : Array[VisitorInstance]
+
+# Takes and adds a VisitorInstance to the queue, and sorts the queue chronologically
+func add_visitor_to_queue(visitor : VisitorInstance):
+	visitor_queue.append(visitor)
+	visitor_queue.sort_custom(func(a, b): return a.get_visit_time < b.get_visit_time)
+
 
 
 func on_day_change():
+	temp() # I Can't think of a good method name
+
+
+# Requires visitor_queue to be sorted chronologically
+# Summons visitors scheduled for the current time
+func temp():
 	for visitor in visitor_queue:
-		if day is correct:
-			send them in
+		if visitor.get_visit_time() > time:
+			break
+		if visitor.get_visit_time() == time:
+			"Summon them"
 
-func populate_queue():
-	add daily visitors to queue
 
-func flag_changer(visitor_name : String, flag : String, state):
-	visitor.info[flag] = state
-	
-	
-###
-#detective: 
-	#info = {
-		#"cultists arrested" : false
-	#}
-#
-#cultist:
-	#on_being_tricked:
-		#VisitorManager.flag_changer("detective", "cultists arrested", true)
-###
+#func flag_changer(visitor_name : String, flag : String, state):
+	#visitor.info[flag] = state
