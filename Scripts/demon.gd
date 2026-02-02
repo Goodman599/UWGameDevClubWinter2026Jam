c1d7d448f7f4aa02253398625e7f8c07536ce1d6
ArmandMeyer <armand03@uw.edu>
2026-02-02 11:44:53 -0800

    work in progress

---
+++ Scripts/demon.gd
@@ -0,0 +1,19 @@
+extends BaseVisitor
+
+# The Demon.
+
+func _init():	
+	visitor_states = {
+		
+	}
+
+
+func _ready(): 
+	super()
+	# add first visit to queue
+	var next_visit = VisitorInstance.new()
+	next_visit.person = self
+	next_visit.visit_branch = "visit0"
+	next_visit.visit_time = 0
+	
+	VisitorManager.add_visitor_to_queue(next_visit)
